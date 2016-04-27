a7f228f7f7f3650d3819ea2c84a9646f7e1a3853
Your Name <your-address@example.com>
2016-04-27 17:53:43 -0400

    Add new files

---
+++ lesson2-1/Task3.py
@@ -0,0 +1,10 @@
+a = (1,2,3,3,5,5,5,1,1)
+b = (1, 'a',2,3,2,'a')
+
+set1 = set(a)
+rez = list(set1)
+print(rez)
+
+set = set(b)
+rez2 = list(set)
+print(rez2)
---
+++ lesson2-1/Task4.py
@@ -0,0 +1,6 @@
+a = 'hi higggggghi hihi hi'
+
+print a.replace('hi', 'bye',0)
+
+print "count hi - ", a.count('hi')
+
